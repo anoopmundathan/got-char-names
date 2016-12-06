0bdee3a2f9732795ec116e650fd707f0ed41a553
anoopmundathan <anoop.mundathan@gmail.com>
2016-12-06 17:15:31 +0000

    Add Test coverage using Mocha

--- src/index.test.js
+++ src/index.test.js
@@ -1,8 +1,30 @@
 var gotNames = require('./index');
 var expect = require('chai').expect;
 
-describe('Got Names', function() {
-	it('Should Work', function() {
-		expect(true).to.be.true
+describe('Got Char Names', function() {
+	
+	describe('all', function() {
+		
+		it('should be an array of strings', function() {
+			expect(gotNames.all).to.satisfy(isArrayOfStrings);
+
+			function isArrayOfStrings(array) {
+				return array.every(function(item) {
+					return typeof item === 'string';
+				});
+			} 
+		}); 
+
+		it('should contain `Arya Stark`', function() {
+			expect(gotNames.all).to.include('Arya Stark');
+		});
 	});
+
+	describe('random', function() {
+		it('should return a random name from got names array', function() {
+			var randomItem = gotNames.random();
+			expect(gotNames.all).to.include(randomItem);
+		});
+	});
+
 });
